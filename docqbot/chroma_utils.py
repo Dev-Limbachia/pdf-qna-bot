23745a76b19fee62aa1bd70bbf937ca735d221ef
Dev-Limbachia <bplfamily21@gmail.com>
2025-06-26 23:27:06 +0100

    Updated requirements.txt location

--- docqbot/chroma_utils.py
+++ docqbot/chroma_utils.py
@@ -1,12 +1,12 @@
 import chromadb
 
-client = chromadb.Client()  # Embedded mode only for this version
-
+client = chromadb.Client()
 collection = client.get_or_create_collection("pdf_chunks")
 
 def store_and_query_chunks(chunks, embeddings, question):
     ids = [str(i) for i in range(len(chunks))]
-    collection.delete(ids=ids)
+    collection.delete(ids=ids)  # clear previous session
     collection.add(documents=chunks, embeddings=embeddings, ids=ids)
     results = collection.query(query_texts=[question], n_results=4)
     return results['documents'][0]
+
